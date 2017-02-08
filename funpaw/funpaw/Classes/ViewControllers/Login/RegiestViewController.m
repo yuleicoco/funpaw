cff0c60120c860e034f2a14b9cb769465c44ef5f
ChenZeXuan <610121499@qq.com>
2017-02-08 18:12:25 +0800

    .,.,.,.

--- funpaw/funpaw/Classes/ViewControllers/Login/RegiestViewController.m
+++ funpaw/funpaw/Classes/ViewControllers/Login/RegiestViewController.m
@@ -45,11 +45,19 @@
     
     _emailTextfield = [[UITextField alloc]init];
     _emailTextfield.textColor =YELLOW_COLOR;
+    _emailTextfield.tintColor = YELLOW_COLOR;
+    _emailTextfield.placeholder = @"Enter your email";
+    [_emailTextfield setValue:YELLOW_COLOR forKeyPath:@"_placeholderLabel.textColor"];
+    _emailTextfield.font = [UIFont systemFontOfSize:18];
+    [firstView addSubview:_emailTextfield];
+    [_emailTextfield mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.equalTo(_emailTextfield.superview.mas_left).offset(10);
+        make.width.mas_equalTo(250);
+        make.height.mas_equalTo(48);
+        make.centerY.mas_equalTo(_emailTextfield.superview.mas_centerY);
+    }];
     
-    
-    
-    
-    
+
     _codeBtn = [[UIButton alloc]init];
     _codeBtn.backgroundColor = RGB(245, 145, 40);
     [_codeBtn setTitle:@"Send Code" forState:UIControlStateNormal];
@@ -80,10 +88,91 @@
 
     }];
     
+    _codeTextfield = [[UITextField alloc]init];
+    _codeTextfield.tintColor = YELLOW_COLOR;
+    _codeTextfield.textColor = YELLOW_COLOR;
+    _codeTextfield.placeholder = @"Enter your verification code";
+    [_codeTextfield setValue:YELLOW_COLOR forKeyPath:@"_placeholderLabel.textColor"];
+    _codeTextfield.font = [UIFont systemFontOfSize:18];
+    [secoendView addSubview:_codeTextfield];
+    [_codeTextfield mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.equalTo(_codeTextfield.superview.mas_left).offset(10);
+        make.width.mas_equalTo(250);
+        make.height.mas_equalTo(48);
+        make.centerY.mas_equalTo(_codeTextfield.superview.mas_centerY);
+
+    }];
     
+    UIView * passView = [[UIView alloc]init];
+    passView.backgroundColor =  [UIColor whiteColor];
+    passView.layer.borderColor = [UIColor redColor].CGColor;
+    passView.layer.borderWidth = 1;
+    passView.layer.cornerRadius = 25;
+    [self.view addSubview:passView];
+    [passView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.equalTo(passView.superview).offset(15);
+        make.right.equalTo(passView.superview).offset(-15);
+        make.top.equalTo(secoendView.mas_bottom).offset(15);
+        make.height.mas_equalTo(50);
+
+    }];
     
+    _passwordTextfield = [[UITextField alloc]init];
+    _passwordTextfield.tintColor = YELLOW_COLOR;
+    _passwordTextfield.textColor = YELLOW_COLOR;
+    _passwordTextfield.placeholder = @"Enter password(at least 6 digits)";
+    [_passwordTextfield setValue:YELLOW_COLOR forKeyPath:@"_placeholderLabel.textColor"];
+    [passView addSubview:_passwordTextfield];
+    [_passwordTextfield mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.equalTo(_passwordTextfield.superview.mas_left).offset(10);
+        make.width.mas_equalTo(250);
+        make.height.mas_equalTo(48);
+        make.centerY.mas_equalTo(_passwordTextfield.superview.mas_centerY);
+        
+    }];
     
+    UIView * surepassView = [[UIView alloc]init];
+    surepassView.backgroundColor = [UIColor whiteColor];
+    surepassView.layer.borderWidth = 1;
+    surepassView.layer.borderColor = [UIColor redColor].CGColor;
+    surepassView.layer.cornerRadius = 25;
+    [self.view addSubview:surepassView];
+    [surepassView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.equalTo(surepassView.superview).offset(15);
+        make.right.equalTo(surepassView.superview).offset(-15);
+        make.top.equalTo(passView.mas_bottom).offset(15);
+        make.height.mas_equalTo(50);
+
+        
+    }];
     
+    _surePasswordfield = [[UITextField alloc]init];
+    _surePasswordfield.textColor = YELLOW_COLOR;
+    _surePasswordfield.tintColor = YELLOW_COLOR;
+    _surePasswordfield.placeholder = @"Re-enter password";
+    [_surePasswordfield setValue:YELLOW_COLOR forKeyPath:@"_placeholderLabel.textColor"];
+    [surepassView addSubview:_surePasswordfield];
+    [_surePasswordfield mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.equalTo(_surePasswordfield.superview.mas_left).offset(10);
+        make.width.mas_equalTo(250);
+        make.height.mas_equalTo(48);
+        make.centerY.mas_equalTo(_surePasswordfield.superview.mas_centerY);
+    }];
+    
+    UIButton * registBtn = [[UIButton alloc]init];
+    registBtn.backgroundColor = YELLOW_COLOR;
+    [registBtn setTitle:@"Register" forState:UIControlStateNormal];
+    [registBtn setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+    registBtn.titleLabel.font = [UIFont systemFontOfSize:18];
+    [self.view addSubview:registBtn];
+    [registBtn mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.equalTo(registBtn.superview).offset(15);
+        make.right.equalTo(registBtn.superview).offset(-15);
+        make.top.equalTo(surepassView.mas_bottom).offset(25);
+        make.height.mas_equalTo(50);
+
+
+    }];
     
     
     
